367389fd8c2ae4425f1e220ec573274b84c7e56c
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-18 20:33:42 -0300

    feat: add validation if no rate is selected

--- src/js/index.js
+++ src/js/index.js
@@ -6,21 +6,37 @@ let rateInfo = document.querySelector('.thankYouCard__indexInfoSection__indexInf
 let rate = 0
 
 ratingButtons.forEach((button, index) => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
 
-        ratingButtons.forEach(button =>{
+        ratingButtons.forEach(button => {
             button.classList.remove('mainCard__ratingSection__buttonsContainer__button--selected')
         })
 
         button.blur()
         button.classList.add('mainCard__ratingSection__buttonsContainer__button--selected')
+        submitButton.classList.remove('mainCard__ratingSection__submitButton--no-rate-selected')
+        submitButton.classList.remove('mainCard__ratingSection__submitButton--no-rate-selected-shake')
         submitButton.focus()
 
         rate = index + 1
     })
 })
 
-submitButton.addEventListener('click', function(){
+submitButton.addEventListener('click', function () {
+
+    if (rate === 0) {
+
+        submitButton.classList.remove('mainCard__ratingSection__submitButton--no-rate-selected-shake'
+
+        )
+        void submitButton.offsetWidth
+        submitButton.classList.add('mainCard__ratingSection__submitButton--no-rate-selected-shake')
+
+        submitButton.blur()
+        ratingButtons[4].focus()
+        return
+    }
+
     mainCard.classList.add('hidden')
     thankYouCard.classList.remove('hidden')
 
