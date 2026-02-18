9922c4add3c6ca098dda5af981061b54a9b83179
Rodrigo Costa <rodrigojesusdacosta@gmail.com>
2026-02-18 19:57:49 -0300

    feat: add functional javascript

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const mainCard = document.querySelector('.mainCard')
+const thankYouCard = document.querySelector('.thankYouCard')
+const ratingButtons = document.querySelectorAll('.mainCard__ratingSection__buttonsContainer__button')
+const submitButton = document.querySelector('.mainCard__ratingSection__submitButton')
+let rateInfo = document.querySelector('.thankYouCard__indexInfoSection__indexInfoContainer__indexInfo__indexNumber')
+let rate = 0
+
+ratingButtons.forEach((button, index) => {
+    button.addEventListener('click', function(){
+
+        ratingButtons.forEach(button =>{
+            button.classList.remove('mainCard__ratingSection__buttonsContainer__button--selected')
+        })
+
+        button.classList.add('mainCard__ratingSection__buttonsContainer__button--selected')
+
+        rate = index + 1
+    })
+})
+
+submitButton.addEventListener('click', function(){
+    mainCard.classList.add('hidden')
+    thankYouCard.classList.remove('hidden')
+
+    rateInfo.textContent = rate
+})
